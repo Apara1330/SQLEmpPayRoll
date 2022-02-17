361eb6a46614d30cb77dd24a22f96e92b22623b6
Apara1330 <polapara1330@gmail.com>
2022-02-17 19:09:21 +0530

    [Apara]- Created emp payroll table

--- SQLQuery[14-02-2022].sql
+++ SQLQuery[14-02-2022].sql
@@ -2,6 +2,13 @@
 create database payroll_service;
 show database;
 use payroll_service;
+----------UC2-------------------
+CREATE TABLE employee_payroll1 (
+    Id int IDENTITY(1,1) PRIMARY KEY,
+    Name varchar(200),
+    SALARY float,
+	DATE date
+);
 
 
 
