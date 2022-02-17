ed1217b41a729c08769518bcedb74f18e3ce05a2
Apara1330 <polapara1330@gmail.com>
2022-02-17 19:22:13 +0530

    [Apara]- Inserted data in to the table

--- SQLQuery[14-02-2022].sql
+++ SQLQuery[14-02-2022].sql
@@ -9,6 +9,13 @@ CREATE TABLE employee_payroll1 (
     SALARY float,
 	DATE date
 );
+-----------UC3------------------
+insert into employee_payroll1 values('Apara',50000,'2021-12-15'),
+('Sidhu',60000,'2011-11-05'),
+('Gautam',75000,'2015-10-25'),
+('Rupali',66000,'2016-09-14'),
+('Depali',20000,'2017-10-23'),
+('Aditya',34000,'2021-07-07');
 
 
 
