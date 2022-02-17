7864c228ecc3a2524688492258262ef0460993c2
Apara1330 <polapara1330@gmail.com>
2022-02-17 20:08:58 +0530

    [Apara]- Data Retrived from table

--- SQLQuery[14-02-2022].sql
+++ SQLQuery[14-02-2022].sql
@@ -16,6 +16,8 @@ insert into employee_payroll1 values('Apara',50000,'2021-12-15'),
 ('Rupali',66000,'2016-09-14'),
 ('Depali',20000,'2017-10-23'),
 ('Aditya',34000,'2021-07-07');
+-----------UC4------------------
+select * from employee_payroll1;
 
 
 
